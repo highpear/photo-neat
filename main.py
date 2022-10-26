4a9ecaca84bb493e644a020793e9f245b9eb6d33
rkt <highpear.0619@gmail.com>
2022-10-26 14:48:50 +0900

    関数名get_all_files()をretrieve_img_path()に変更

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from rename import *
 
 
 # dir以下の指定拡張子のファイルパスをリストで取得
-def get_all_files(dir, TARGET_EXT=[], recursive=True, includeAAE=False):
+def retrieve_img_path(dir, TARGET_EXT=[], recursive=True, includeAAE=False):
 
     if includeAAE:                # 写真編集履歴ファイルを含める場合
         TARGET_EXT.append('aae')
@@ -238,7 +238,7 @@ def main():
         sys.exit()
 
     # 対象ファイルのパスを取得
-    fpath_list = get_all_files(SRC_DIR, TARGET_EXT=TARGET_EXT, recursive=RECURSIVE)
+    fpath_list = retrieve_img_path(SRC_DIR, TARGET_EXT=TARGET_EXT, recursive=RECURSIVE)
     print(f'{len(fpath_list)}件のファイルが見つかりました')
 
     # 入力元フォルダからファイルをコピー
@@ -253,7 +253,7 @@ def main():
     ######################
     if mode == 'clsby':
         print('指定されたフォルダ以下の全ファイルを分類します')
-        fpath_list = get_all_files(SRC_DIR, TARGET_EXT=TARGET_EXT)
+        fpath_list = retrieve_img_path(SRC_DIR, TARGET_EXT=TARGET_EXT)
         cls_mode = args.arg2
 
         # 拡張子でフォルダ分け
@@ -290,7 +290,7 @@ def main():
     ##################
     elif mode == 'renby':  # リネーム処理
         print('指定されたフォルダ以下の全ファイルをリネームします')
-        fpath_list = get_all_files(SRC_DIR, TARGET_EXT=TARGET_EXT)
+        fpath_list = retrieve_img_path(SRC_DIR, TARGET_EXT=TARGET_EXT)
 
         ren_method = ['REPLACEALL', '']  # リネーム方式はデフォルトで全置換
         if args.renmethod:
